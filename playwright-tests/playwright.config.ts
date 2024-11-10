aa05ea2eae9a22aa8fc13fcce129fbf2a977b677
DuyguT88 <duygu.baydi@gmail.com>
2024-11-10 21:26:23 +0100

    updated html report generation folder

--- playwright-tests/playwright.config.ts
+++ playwright-tests/playwright.config.ts
@@ -7,18 +7,19 @@ const config: PlaywrightTestConfig = {
     headless: true,
     viewport: { width: 1280, height: 720 },
     actionTimeout: 5000,
+    trace: 'on-first-retry' // Collect a trace on first retry
   },
   projects: [
     { name: 'Chromium', use: { browserName: 'chromium' } },
-    { name: 'Firefox', use: { browserName: 'firefox' } }
-    //{ name: 'WebKit', use: { browserName: 'webkit' } }
+    { name: 'Firefox', use: { browserName: 'firefox' } },
+    { name: 'WebKit', use: { browserName: 'webkit' } }
   ],
   reporter: [
     //['dot'],  // Simple console output
-    ['json', { outputFile: 'test-results/JSON/results-json.json' }],  // JSON report
+    ['json', { outputFile: 'test-results/JSON/ui-test-json.json' }],  // JSON report
     ['junit', { outputFile: 'test-results/XML/ui-test-results-xml.xml' }],  // JUnit XML report
-    ['html', { outputFolder: 'test-results/HTML/ui-tests-html-report' }]  // HTML report
+    ['html', { outputFolder: 'ui-tests-html-report' }]  // HTML report in a separate folder, since HTML reporter will clear its output directory before generating the report, leading to potential data loss if both directories overlap.
   ]
 };
 
-export default config;
+export default config;W
